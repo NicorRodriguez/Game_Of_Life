ff4644a5c5860e2f98aad07e2323b7be6d8fa306
NicorRodriguez <nicolas@nicolass-MacBook-Air-579.local>
2022-09-01 09:15:49 -0300

    My commit?

--- src/Library/Class1.cs
+++ src/Library/Class1.cs
@@ -1,8 +1,67 @@
-﻿using System;
+﻿using System.IO;
 
-namespace PII_Game_Of_Life
-{
-    public class Class1
-    {
+namespace PII_Game_Of_Life{
+
+    public class celula{
+        public bool state {get; set;}
+
+        public bool imAlive(int neighboursAlive){
+            if(state && neighboursAlive<2) this.state = false;
+            if(state && neighboursAlive>3) this.state = false;
+            if(!state && neighboursAlive==3) this.state = false;
+            if(state && (neighboursAlive ==2 || neighboursAlive ==3) ) this.state = true;
+            return this.state;
+        }
+    }
+    public class Board{
+        // public static celula[,] newBoard(int width, int height){ return new celula[width, height];}
+        public celula[,] board;
+        int boardWidth;
+        int boardHeight;
+
+        public celula[,] getBoard(){ return this.board; }
+
+        public int width(){ return boardWidth; }
+        public int height(){ return boardHeight; }
+
+        public void importBoard(string path){
+            string url = path;
+            string content = File.ReadAllText(url);
+            string[] contentLines = content.Split('\n');
+            this.boardHeight = contentLines.Length;
+            this.boardWidth = contentLines[0].Length;
+            celula[,] myBoard = new celula[contentLines.Length, contentLines[0].Length];
+            for (int  y=0; y<contentLines.Length;y++){
+                for (int x=0; x<contentLines[y].Length; x++){
+                    celula mycell = new celula();
+                    mycell.state = false;
+                    if(contentLines[y][x]=='1'){ mycell.state = true; }
+                    myBoard[x,y]=mycell;
+                }
+            }
+
+            this.board = myBoard;
+        }
+
+        public int getNeighbours(int posX, int posY){
+
+            int aliveNeighbors = 0;
+            for (int i = posX-1; i<=posX+1;i++){
+                for (int j = posY-1;j<=posY+1;j++){
+                    if(i>=0 && i<boardWidth && j>=0 && j < boardHeight && this.board[i,j].state)
+                    {
+                        aliveNeighbors++;
+                    }
+                }
+            }
+
+            return aliveNeighbors;
+            
+        }
+
+    }
+
+    public class Game{
+
     }
 }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
 
-namespace PII_Game_Of_Life
-{
-    class Program
-    {
+namespace PII_Game_Of_Life{
+    class Program{
+        public static void showBoard(Board myBoard){
+            StringBuilder s = new StringBuilder();
+            Console.Clear();
+            for (int y = 0; y<myBoard.height();y++)
+            {
+                for (int x = 0; x<myBoard.width(); x++)
+                {
+                    if(myBoard.getBoard()[x, y].state)
+                    {
+                        s.Append("|X|");
+                    }
+                    else
+                    {
+                        s.Append("___");
+                    }
+                }
+                s.Append("\n");
+            }
+            Console.WriteLine(s.ToString());
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Board myBoard = new Board();
+            myBoard.importBoard("../../assets/board2.txt");
+
+            Board myNewBoard = new Board();
+
+            myNewBoard.importBoard("../../assets/board2.txt");
+
+            showBoard(myBoard);
+
+            Console.WriteLine(myBoard.height());
+            Console.WriteLine(myBoard.width());
+
+            while(true){
+                for(int x = 0; x < myBoard.width(); x++){
+                    for(int y = 0; x < myBoard.height(); y++){
+
+                        int aliveNeighbors = myBoard.getNeighbours(x, y);
+
+                        myNewBoard.board[x,y].state = myBoard.board[x,y].imAlive(aliveNeighbors);
+
+
+                    }
+                }
+
+                myBoard = myNewBoard;
+
+                showBoard(myBoard);
+
+                Thread.Sleep(300);
+            }
         }
+
+        
     }
 }
